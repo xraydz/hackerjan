9bd7fce1502c08e4fc75449178ce24372ddbe2e4
10xray01 <110544522+xraydz@users.noreply.github.com>
2025-08-13 18:35:30 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   base: '/rce/',
+  trailingSlash: 'always',
+  build: { format: 'directory' },
   outDir: 'dist'
 });
